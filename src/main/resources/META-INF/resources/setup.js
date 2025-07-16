320dc436890ebdfcac38a94b2691e96616713dc1
Mario Harrer <mario.harrer@feratel.at>
2025-07-16 11:33:56 +0200

    Add player selection validation and dynamic input handling in `setup.js`
    - Added max player selection validation with user feedback for invalid choices.
    - Enabled dynamic creation of hidden inputs for selected players on configuration.

--- src/main/resources/META-INF/resources/setup.js
+++ src/main/resources/META-INF/resources/setup.js
@@ -4,11 +4,42 @@ document.addEventListener('DOMContentLoaded', function() {
     const modeSelect = document.getElementById('modeSelect');
     const mode501Config = document.getElementById('mode501Config');
     const clockConfig = document.getElementById('clockConfig');
+    const playerSelect = document.querySelector('select[name="players"]');
 
+    configureButton.disabled = true;
+    playerSelect.addEventListener('change', function() {
+        const selected = Array.from(this.selectedOptions).length;
+
+        if(selected > 0 && selected <= 4){
+            configureButton.disabled = false;
+        }
+        else{
+            configureButton.disabled = true;
+
+            Swal.fire({
+                title:"Ungültige auswahl",
+                text: "Maximal 4 Spieler sind erlaubt",
+                icon: 'error',
+                toast: true,
+                position: 'top-end',
+                timer: 2000,
+                showConfirmButton: false
+            })
+        }
+    })
     if(configureButton && modeConfigOptions)
         configureButton.addEventListener('click', function() {
             modeConfigOptions.style.display = 'block';
             updateModeConfig();
+            const selectedArray = Array.from(playerSelect.selectedOptions);
+            selectedArray.forEach(option => {
+                const hide = document.createElement('input');
+                hide.type = 'hidden';
+                hide.name = 'players';
+                hide.value = option.value;
+                playerSelect.form.appendChild(hide)
+            })
+            playerSelect.disabled = true;
         });
 
     function updateModeConfig() {
